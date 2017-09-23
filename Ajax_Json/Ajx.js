76bbfcd43d75075ac38beea9e7940be8c9df3835
Ričardas Staniulis <Viproshal@gmail.com>
2017-09-23 23:06:11 +0100

    Add files via upload

--- Ajax_Json/Ajx.js
+++ Ajax_Json/Ajx.js
@@ -1,8 +1,27 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+var pageCounter = 1;
+var btn = document.getElementById("btn");
+var animalContainer = document.getElementById("animal-info");
+
+btn.addEventListener("click",function(){
+	var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-' + pageCounter+'.json');
 
 ourRequest.onload = function(){
 	var ourData = JSON.parse(ourRequest.responseText);
-	console.log(ourData[0].name)
+	renderHTML(ourData);
 };
  ourRequest.send();
+ pageCounter++;
+ if(pageCounter >3){
+ 	btn.classList.add("hide-me");
+ }
+});
+
+function renderHTML(data) {
+	var htmlString = "";
+
+	for(r=0;r<data.length;r++){
+		htmlString += "<p>"+data[r].name + " is a "+  data[r].species+"</p>";
+	}
+	animalContainer.insertAdjacentHTML('beforeend',htmlString);
+}
